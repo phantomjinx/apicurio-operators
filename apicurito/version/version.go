c25d41a31aeeb6255d665aa8e419a1b193e24aef
Paul <parichar@redhat.com>
2022-03-18 18:40:02 +0000

    Bump version to 7.11 (1.11)
    * Previous version -> 7.10.2

--- apicurito/version/version.go
+++ apicurito/version/version.go
@@ -2,6 +2,6 @@ package version
 
 var (
 	// these version are set at build time, see scripts/go-build.sh, the version values come from config/vars/Makefile
-	Version      = "7.10.1"
-	PriorVersion = "7.10.0"
+	Version      = "7.11.0"
+	PriorVersion = "7.10.1"
 )
