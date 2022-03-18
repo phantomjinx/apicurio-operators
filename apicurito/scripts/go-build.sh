2425999a258f211aafcd63fc602fdc27f043f7a1
Paul <parichar@redhat.com>
2022-03-18 16:04:26 +0000

    Chore: Updates build script to use Makefile
    * checks on env vars being set
    
    * GOFLAGS added for git-commit and build time

--- apicurito/scripts/go-build.sh
+++ apicurito/scripts/go-build.sh
@@ -1,26 +1,65 @@
 #!/bin/bash
-REGISTRY=quay.io/${USER}
-IMAGE=apicurito-operator
-TAG=v1.1.0
-GIT_COMMIT=$(git rev-list -1 HEAD)
-FUSE_VERSION=$(grep ^DEFAULT_VERSION ./config/vars/Makefile|awk '{print $3}')
-FUSE_PREV_VERSION=$(grep ^DEFAULT_PREVIOUS_VERSION ./config/vars/Makefile|awk '{print $3}')
-export GO111MODULE=on
-GOFLAGS="-X github.com/apicurio/apicurio-operators/apicurito/pkg/cmd.GitCommit=${GIT_COMMIT}"
-GOFLAGS=$GOFLAGS" -X github.com/apicurio/apicurio-operators/apicurito/version.Version=${FUSE_VERSION}"
-GOFLAGS=$GOFLAGS" -X github.com/apicurio/apicurio-operators/apicurito/version.PriorVersion=${FUSE_PREV_VERSION}"
+
+check_env_var() {
+  if [ -z "${2}" ]; then
+    echo "Error: ${1} env var not defined"
+    exit 1
+  fi
+}
+
+while getopts :b:g:i:p:t:v: opt; do
+  case $opt in
+    b)
+      BUILD_TIME=${OPTARG}
+      ;;
+    g)
+      GIT_COMMIT=${OPTARG}
+      ;;
+    i)
+      IMAGE=${OPTARG}
+      ;;
+    p)
+      PREV_VERSION=${OPTARG}
+      ;;
+    t)
+      TAG=${OPTARG}
+      ;;
+    v)
+      VERSION=${OPTARG}
+      ;;
+  esac
+done
+
+# Remove processed options from arguments
+shift $(( OPTIND - 1 ));
+
+check_env_var "BUILD_TIME" ${BUILD_TIME}
+check_env_var "IMAGE" ${IMAGE}
+check_env_var "PREV_VERSION" ${PREV_VERSION}
+check_env_var "TAG" ${TAG}
+check_env_var "VERSION" ${VERSION}
 
 # Updates version/version.go
-sed -i "s/Version      = .*/Version      = \"${FUSE_VERSION}\""/ version/version.go
-sed -i "s/PriorVersion = .*/PriorVersion = \"${FUSE_PREV_VERSION}\""/ version/version.go
+sed -i "s/Version      = .*/Version      = \"${VERSION}\""/ version/version.go
+sed -i "s/PriorVersion = .*/PriorVersion = \"${PREV_VERSION}\""/ version/version.go
 
 go mod vendor
 go generate ./...
 
 ./scripts/go-test.sh
 
+echo
+echo "=== Compiling app ..."
+export GO111MODULE=on
+GOFLAGS="-X github.com/apicurio/apicurio-operators/apicurito/version.Version=${VERSION}"
+GOFLAGS=${GOFLAGS}" -X github.com/apicurio/apicurio-operators/apicurito/version.PriorVersion=${PREV_VERSION}"
+GOFLAGS=${GOFLAGS}" -X github.com/apicurio/apicurio-operators/apicurito/pkg.Version=${VERSION}"
+GOFLAGS=${GOFLAGS}" -X github.com/apicurio/apicurio-operators/apicurito/pkg.BuildDateTime=${BUILD_TIME}"
+GOFLAGS=${GOFLAGS}" -X github.com/apicurio/apicurio-operators/apicurito/pkg/cmd.GitCommit=${GIT_COMMIT}"
+
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 \
-  go build -ldflags "${GOFLAGS}" \
+go build -a \
+  -ldflags "${GOFLAGS}" \
   -o build/_output/bin/apicurito \
   -mod=vendor github.com/apicurio/apicurio-operators/apicurito/cmd/manager
 if [ $? != 0 ]; then
@@ -28,4 +67,4 @@ if [ $? != 0 ]; then
   exit 1
 fi
 
-docker build . -t $REGISTRY/$IMAGE:$TAG
+docker build . -t ${IMAGE}:${TAG}
