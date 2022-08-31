640ed810a76d4d07312fed825691a4c911e45c64
phantomjinx <p.g.richardson@phantomjinx.co.uk>
2022-08-31 15:52:02 +0100

    Bump version to 7.11.1 (1.11.1)
      * Previous version -> 7.11.0

--- apicurito/version/version.go
+++ apicurito/version/version.go
@@ -4,8 +4,8 @@ import "strings"
 
 var (
 	// these version are set at build time, see scripts/go-build.sh, the version values come from config/vars/Makefile
-	Version      = "7.11.0"
-	PriorVersion = "7.10.2"
+	Version      = "7.11.1"
+	PriorVersion = "7.11.0"
 )
 
 // ShortVersion return the major.minor, as 7.8, instead of 7.8.0
